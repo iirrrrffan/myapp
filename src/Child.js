390cdf6df5bb7e8c5c346392518edf6651330437
iirrrrffan <irfanali376246@gmail.com>
2024-02-29 10:06:03 +0530

    push

--- src/Child.js
+++ src/Child.js
@@ -4,14 +4,20 @@ import { Kk, kk } from './Components/Test1';
 
 const Child = () => {
   const [count,setCount]=useState(0)
-  console.log("parant ....");
+const incri = ()=>{
+  setCount(count+9)
+}
   return (
     <div>
-      <button onClick={()=>setCount((prev)=>prev+1)}>+</button>
+      <button onClick={incri}>+</button>
     <h1>{count}</h1>
     <Kk/>
     </div>
   )
-}
 
+
+
+
+  
+}
 export default Child
